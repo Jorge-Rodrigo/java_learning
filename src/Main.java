3e670be683ae7f9ab3501ef3a11334ee85c49b07
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2026-05-28 20:00:02 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// int x, y;
+		
+		// x = 5;
+		// y = 2 * x;
+		
+		// System.out.println(x);
+		// System.out.println(y);
+		
+		/* int x2;
+		double y2;
+		
+		x2 = 5;
+		y2 = 2 * x2;
+		
+		System.out.println(x2);
+		System.out.println(y2);
+		*/
+		
+	}
+}
