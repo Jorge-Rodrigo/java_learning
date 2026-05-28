cb167608db850a745e020030d846c9b4c586b90f
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2026-05-28 20:02:44 -0300

    feat: double calculator

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,15 @@ public class Main {
 		System.out.println(y2);
 		*/
 		
+		
+		double b, B, h , area;
+		
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
+		
 	}
 }
